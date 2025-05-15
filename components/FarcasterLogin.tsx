7bdf93288fabeea4ac812c2233ab0fbc1fd02f3c
Morpheus42069 <morpheus69420@proton.me>
2025-05-14 23:43:00 -0400

    fix farcaster login siwn variant

--- components/FarcasterLogin.tsx
+++ components/FarcasterLogin.tsx
@@ -1,11 +1,11 @@
 'use client';
 
-import { NeynarAuthButton } from '@neynar/react';
+import { NeynarAuthButton, SIWN_variant } from '@neynar/react';
 
 export default function FarcasterLogin() {
   return (
     <div style={{ marginTop: '2rem' }}>
-      <NeynarAuthButton label="Login with Farcaster" variant="farcaster" />
+      <NeynarAuthButton label="Login with Farcaster" variant={SIWN_variant.farcaster} />
     </div>
   );
 }
